a65465734a63830a2aa95fab76910c44338c8205
VLipatov <lipatov_xexA>
2016-07-01 15:50:34 +0300

    Вроде бы, готов теперь

--- create_base.py
+++ create_base.py
@@ -3,22 +3,21 @@ number = int(input())
 while True:
     print('Введи текст (для выхода введи 0)')
     text = input()
-    if text=='0':
+    if text == '0':
         break
-    print('Введи жанр цифрой(Художественный - 0, научный - 1, публицистический - 2')
+    print('Введи жанр цифрой(Художественный - 0, научный - 1, публицистический - 2)')
     genre = int(input())
     file_name = 'Sample_Text_' + str(number) + '.txt'
     file = open(file_name, 'w')
     file.write(text)
     file.close()
     answer_name = 'Genre_' + str(number) + '.txt'
-    file = open(answer_name, 'w')
+    file = open(answer_name)
     if genre == 0:
-        file.write('Художественный')
+        file.write(u'Художественный')
     elif genre == 1:
-        file.write('научный')
+        file.write(u'научный')
     elif genre == 2:
-        file.write('публицистический')
+        file.write(u'публицистический')
     file.close()
-    number+=1
-
+    number += 1
